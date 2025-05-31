f328c0130c55cb478e48d99fb4644d5a67c7ea8c
Aasish B Vijayan <aasishvijayan@gmail.com>
2025-05-31 22:40:37 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Aasish B Vijayan",
+    author_email="aasishvijayan@gmail.com",
+    packages=find_packages()
+)
